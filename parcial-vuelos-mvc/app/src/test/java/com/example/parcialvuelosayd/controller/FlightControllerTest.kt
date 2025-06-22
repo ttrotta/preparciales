ed20f9bf1693002d5657c946805ed6c66efe26e1
Thiago Trotta <trottathiago1@gmail.com>
2025-06-22 19:28:39 -0300

    Add controller test

--- parcial-vuelos-mvc/app/src/test/java/com/example/parcialvuelosayd/controller/FlightControllerTest.kt
+++ parcial-vuelos-mvc/app/src/test/java/com/example/parcialvuelosayd/controller/FlightControllerTest.kt
@@ -1,5 +1,28 @@
 package com.example.parcialvuelosayd.controller
 
+import ayds.observer.Subject
+import com.example.parcialvuelosayd.model.FlightModel
+import com.example.parcialvuelosayd.view.FlightView
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Test
+
 class FlightControllerTest {
 
+    private val flightModel: FlightModel = mockk()
+
+    private val onActionSubject = Subject<String>()
+    private val flightView: FlightView = mockk(relaxUnitFun = true) {
+        every { observerView } returns onActionSubject
+    }
+
+    private val controller = FlightControllerImpl(flightView, flightModel)
+
+    @Test
+    fun `on a view action should fetch the flights`() {
+        controller.initObserver()
+        onActionSubject.notify("Argentina")
+        verify(timeout = 1_000) { flightModel.fetchVuelos("Argentina") }
+    }
 }
